0e3163270a8fe5775f9d9ce01beb3e865c8c4d46
SirJacen <jan-ole.weiss@stud.hs-emden-leer.de>
2022-10-25 16:17:12 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from functions import replacewhite, replaceblack
+
+# set folder name where the images are
+folder = ""
+
+# choose depending on background color
+# only one can be choosen
+replaceblack(folder)
+# replacewithmask(folder)
